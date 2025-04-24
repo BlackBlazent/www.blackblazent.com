23056fdc3ac48f430cfd4067b9bba9c0440d32ce
BlackRose <nissandiezonencaboagripoz9@gmail.com>
2025-04-24 23:26:06 +0800

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mode: 'production',
-    devtool: 'source-map', // This enables source map generation
-    entry: './src/app.js',
-    output: {
-      filename: 'app.min.js',
-    },
-  };
-  
