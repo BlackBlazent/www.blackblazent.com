a1f50f30ff9eb87c429b6e3fadc6156822d20de8
BlackRose <nissandiezonencaboagripoz9@gmail.com>
2025-04-27 23:36:23 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import IconLoader from './components/IconLoader';
 import './index.css'
 import App from './App.tsx'
 
