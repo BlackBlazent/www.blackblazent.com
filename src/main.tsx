f4252e3785412f009e4b9c428b0a7ca75e3403ac
LoneStamp <nissandiezonencaboagripoz9@gmail.com>
2026-09-24 17:43:29 +0800

    refactor: update application entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import IconLoader from './components/IconLoader';
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/tokens.css';
+import './styles/layout.css';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
