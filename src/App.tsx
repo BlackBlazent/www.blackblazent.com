aa334215362403761d84e1c17ce72e14e055459e
LoneStamp <nissandiezonencaboagripoz9@gmail.com>
2026-09-24 17:43:27 +0800

    refactor: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-// App component
-import './App.css'
-import BlackBlazentUI from './blackblazent.ui'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Solutions from './pages/Solutions';
+import Research from './pages/Research';
+import Engineering from './pages/Engineering';
+import Resources from './pages/Resources';
+import Company from './pages/Company';
+import CompanySub from './pages/CompanySub';
+import Privacy from './pages/Privacy';
+import NotFound from './pages/NotFound';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <BlackBlazentUI />
-  )
-}
+    <HashRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:slug" element={<ProductDetail />} />
+          <Route path="solutions" element={<Solutions />} />
+          <Route path="research" element={<Research />} />
+          <Route path="engineering" element={<Engineering />} />
+          <Route path="resources" element={<Resources />} />
+          <Route path="company" element={<Company />} />
+          <Route path="company/:slug" element={<CompanySub />} />
+          <Route path="legal/privacy" element={<Privacy />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
